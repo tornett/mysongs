92a6f619806f9ba4be5d33932ce2e3761048d92d
Todd Ornett <tornett@telecomsys.com>
2018-12-13 14:07:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## mysongs
+
+mysongs is a client-server solution for listing and playing back songs stored in a directory on the server.
+
+Ouf-of-the-box, pnpm install or preferred package manager will install all
+dependencies. It is written with a default configuration for a server runniing on localhost at port 3000. Configuration details are shown later and all values can be overriden.
+
+### Pre-Requisites
+
+#### Store Desired Songs in "songs" Directory
+
+Song tracks should be stored in the "songs" directory with the following filename format:
+
+```
+  Artist-Album-dd-Title
+
+  where dd is the 2 digit track number on the Album (e.g. 01)
+```
+
+The track number is currently ignored but should be included as a placeholder for future capabilities such as supporting album play order.
+
+### API
+
+#### Get library size
+/v1/songcount
+
+Will return JSON containing the range of valid [begin] and [end] song numbers.
+
+#### Get list of songs including metadata with playback URL
+/v1/songs[query parameters]
+  [begin]
+  [end]
+  
+  /v1/songs?begin=5&end=10
+    returns song 5, 6, 7, 8, 9, and 10 as available in the library
+  
+    
+  /v1/songs?begin=4&end=1
+    returns song 4, 3, 2, and 1 as available in the library
+  
+
+The default is to return the JSON metadata of up to 20 songs in the default configuration
+
+#### Default configuration
+
+```
+MYSONGS_SONGS_DIR = 'songs'
+MYSONGS_HOST_NAME = 'localhost'
+MYSONGS_HOST_SCHEME = 'http://'
+MYSONGS_HOST_PORT = 3000
+MYSONGS_DEFAULT_SONG_COUNT = 20
+```
+
+See [dotenv](https://www.npmjs.com/package/dotenv) for overriding these parameters.
+
---
+++ server/index.js
@@ -0,0 +1,104 @@
+/*
+ * mysongs server
+ */
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+
+const MYSONGS_SONGS_DIR = process.env.MYSONGS_SONGS_DIR || 'songs'
+const MYSONGS_HOST_NAME = process.env.MYSONGS_HOST_NAME || 'localhost'
+const MYSONGS_HOST_SCHEME = process.env.MYSONGS_HOST_SCHEME || 'http://'
+const MYSONGS_HOST_PORT = process.env.MYSONGS_HOST_PORT || 3000
+const MYSONGS_DEFAULT_SONG_COUNT = process.env.MYSONGS_DEFAULT_SONG_COUNT || 20
+
+require('dotenv').config()
+
+// for now loading song information entirely in memory,
+// future change would be utilizing a db such as mongodb
+let songs = []
+
+// load the songs directory
+!(() => {
+  let songsFound = true
+  try {
+    songsFound = fs.lstatSync(MYSONGS_SONGS_DIR).isDirectory();
+  } catch (err) {
+    songsFound = false
+  }
+  if (songsFound) {
+    fs.readdir(MYSONGS_SONGS_DIR, (err, items) => {
+      for (let item of items) {
+        // break fields, begin chopping off the filename extension
+        const [artist, album, track, title] = item.split(/[.-]/)
+        songs.push({
+          artist: artist,
+          album: album,
+          track: track,
+          title: title,
+          url: `${MYSONGS_HOST_SCHEME}${MYSONGS_HOST_NAME}:${MYSONGS_HOST_PORT}/${item}`
+        })
+      }
+      console.log(`${songs.length} songs loaded from directory "${MYSONGS_SONGS_DIR}"`)
+    })
+  } else {
+    console.error(`"${MYSONGS_SONGS_DIR}" is not a readable directory, please see README.md`)
+  }
+})()
+
+
+const app = express()
+
+// cors to allow flexibility with accessing resources
+// from clients outside this domain
+app.use(cors())
+
+// songs statically stored and served from SONGS_DIR
+app.use(express.static(MYSONGS_SONGS_DIR))
+
+// Load tracks from songs directory
+app.get('/v1/songs', (req, res) => {
+  let begin = parseInt(req.query.begin) || 1
+  let end = parseInt(req.query.end) || songs.length
+  let requestedSongs = [...songs]
+  let reversed = ''
+
+  // normalize begin and end to within limits of available songs
+  if (begin < 1 || begin > requestedSongs.length) {
+    begin = 1
+  }
+  if (end < 1 || end > requestedSongs.length) {
+    end = requestedSongs.length
+  }
+
+  // reverse order request
+  if (begin > end) {
+    requestedSongs.reverse()
+    const t = begin
+    begin = end
+    end = t
+    reversed = ' reversed'
+  }
+
+  // limit to maximum of 20 songs
+  if (end - begin > MYSONGS_DEFAULT_SONG_COUNT) {
+    end = begin + MYSONGS_DEFAULT_SONG_COUNT
+  }
+
+  console.log(`request songs ${begin} to ${end}${reversed}`)
+
+  res.json(requestedSongs.slice((begin - 1), end))
+})
+
+app.get('/v1/songcount', (req, res) => {
+  console.log(`request songcount => ${songs.length}`)
+  res.json(
+    {
+      begin: 1,
+      end: songs.length
+    }
+  )
+})
+
+app.listen(MYSONGS_HOST_PORT, () => {
+  console.log(`Server running on port ${MYSONGS_HOST_PORT}`)
+})
